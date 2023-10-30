d2b528b1d3b04972fe4ec97d22229c09e05b8717
maruf <dev.maruf112@gmail.com>
2023-10-30 14:02:21 +0600

    not found route created

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,37 @@
-import cors from 'cors'
-import express, { Application, NextFunction, Request, Response } from 'express'
-import httpStatus from 'http-status'
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 
-import routes from './app/routes'
+import routes from './app/routes';
 
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 
-const app: Application = express()
+const app: Application = express();
 
 const corsOptions = {
   origin: true,
   credentials: true,
-}
-app.use('*', cors(corsOptions))
-app.use(cookieParser())
+};
+app.use('*', cors(corsOptions));
+app.use(cookieParser());
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', routes)
+app.use('/api/v1', routes);
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 app.get('/', async (req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.OK).json({
     success: true,
     message: 'Welcome HTTP SERVER',
-  })
-})
-
-export default app
+  });
+});
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: "false",
+    messsage: "not found"
+  });
+});
+export default app;
