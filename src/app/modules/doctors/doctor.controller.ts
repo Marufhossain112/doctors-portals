050dcccfca7d757972dbe98582a5f05ef9d495fd
maruf <dev.maruf112@gmail.com>
2023-10-30 07:39:42 +0600

    doctor getall , getSingle, update, delete done

--- src/app/modules/doctors/doctor.controller.ts
+++ src/app/modules/doctors/doctor.controller.ts
@@ -17,4 +17,72 @@ const createDoctor = async (req: Request, res: Response) => {
         });
     }
 };
-export const doctorController = { createDoctor };
+const getDoctors = async (req: Request, res: Response) => {
+    try {
+        const result = await doctorService.getDoctors();
+        res.status(200).json({
+            status: 200,
+            message: "Doctors fetched successfully.",
+            data: result
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'error',
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+const getDoctor = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await doctorService.getDoctor(id);
+        res.status(200).json({
+            status: 200,
+            message: "Single Doctor fetched successfully.",
+            data: result
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'error',
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+const updateDoctor = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const result = await doctorService.updateDoctor(id, data);
+        res.status(200).json({
+            status: 200,
+            message: " Doctor updated successfully.",
+            data: result
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'error',
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+const deleteDoctor = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await doctorService.deleteDoctor(id);
+        res.status(200).json({
+            status: 200,
+            message: " Doctor deleted successfully.",
+            data: result
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'error',
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+export const doctorController = { createDoctor, getDoctors, getDoctor, updateDoctor, deleteDoctor };
--- src/app/modules/doctors/doctor.services.ts
+++ src/app/modules/doctors/doctor.services.ts
@@ -4,4 +4,37 @@ const createDoctor = async (data: Doctor): Promise<Doctor> => {
     const result = await prisma.doctor.create({ data });
     return result;
 };
-export const doctorService = { createDoctor };
+const getDoctors = async (): Promise<Doctor[] | null> => {
+    const result = await prisma.doctor.findMany({
+        include: {
+            specialization: true
+        }
+    });
+    return result;
+};
+const getDoctor = async (id: string): Promise<Doctor | null> => {
+    const result = await prisma.doctor.findUnique({
+        where: { id },
+        include: {
+            specialization: true
+        }
+    });
+    return result;
+};
+const updateDoctor = async (id: string, paylaod: Partial<Doctor>): Promise<Partial<Doctor>> => {
+    const result = await prisma.doctor.update({
+        where: {
+            id
+        }, data: paylaod
+    });
+    return result;
+};
+const deleteDoctor = async (id: string): Promise<Partial<Doctor>> => {
+    const result = await prisma.doctor.delete({
+        where: {
+            id
+        }
+    });
+    return result;
+};
+export const doctorService = { createDoctor, getDoctors, getDoctor, updateDoctor, deleteDoctor };
